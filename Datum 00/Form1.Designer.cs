a03eacb25d9adea43720c0b2c069e44999682813
brenekv.03 <brenekv.03@A321-15>
2022-03-16 09:30:44 +0100

    DatumCas00 + DatumCas01 rozpracovane

---
+++ Datum 00/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Datum_00
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(29, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(346, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vytvoření instance DateTime\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(29, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(346, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Různý formát výpisu data a času";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(29, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(346, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Aktuální datum a čas";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(29, 256);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(196, 28);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Věk z data narození";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(29, 291);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(196, 27);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Dovršení plnoletosti";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(29, 179);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(346, 36);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Metody pro získání částečných informací";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(31, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Datetime - datum a čas";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(246, 257);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 28);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "24.9.2000";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(246, 293);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 28);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.Text = "24.9.2000";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(29, 331);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(196, 74);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Uplynula karenční doba 30 dnů od zadaného data?";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(246, 377);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 28);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Text = "24.8.2019";
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button8.Location = new System.Drawing.Point(443, 130);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(252, 84);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "Časový interval mezi dvěma okamžiky";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.Location = new System.Drawing.Point(733, 141);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(152, 28);
+            this.textBox4.TabIndex = 16;
+            this.textBox4.Text = "1.1.2000 8:00:00";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox5.Location = new System.Drawing.Point(733, 187);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(152, 28);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.Text = "2.1.2000 10:30:15";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(439, 98);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "TimeSpan - časový interval";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 472);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Datum 00/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datum_00
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ***** Třída DateTime *****
+            //Slouží pro uložení časového okamžiku (nejčastěji vyjádřen jako datum a čas)
+            //Má spoustu metod pro práci s datem a časem
+
+            //Konstruktor má 12 přetížení, my si vyzkoušíme 4
+
+            //Konstruktor bez parametru - Vytvoří datum a čas 1.1. roku 1, čas je 00:00:00
+            DateTime dt1 = new DateTime();
+            MessageBox.Show(dt1.ToString());
+            //Konstruktor s parametry rok, měsíc, den  (čas zůstane 00:00:00)
+            DateTime dt2 = new DateTime(1, 1, 1);
+            MessageBox.Show(dt2.ToString());
+            //Konstruktor s parametry rok, měsíc, den, hodiny, minuty, sekundy)
+            DateTime dt3 = new DateTime(2022, 6, 30, 9, 30, 0);
+            MessageBox.Show(dt3.ToString());
+            //Konstruktor s parametrem počet taktů (tiků) - zadáme 10 milionů
+            DateTime dt4 = new DateTime(10000000);
+            MessageBox.Show(dt4.ToString());
+            //*******************
+            //Vytvoření instance přiřazením konkrétního data a času
+
+            //řetězec parsujeme na DateTime (vyhodí výjimku, když řetězec není zapsán správně)
+
+
+            //metoda TryParse vrací bool, zda se konverze na DateTime podařila, nikdy nedojde k výjimce
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime dt = new DateTime(2012, 4, 9, 15, 6, 48);
+
+            //*** výpis data a času v různých formátech ***
+
+            //MessageBox.Show(dt.ToShortDateString());
+            //MessageBox.Show(dt.ToLongDateString());
+            //MessageBox.Show(dt.ToShortTimeString());
+            //MessageBox.Show(dt.ToLongTimeString());
+
+            //***Výpis data a času v libovolném tvaru - pomocí formátovacího řetězce ***
+            
+            dt = DateTime.Now;
+            //MessageBox.Show(dt.ToString("dddd d. MM. yyyy h:m:s.fff"));
+            //MessageBox.Show(dt.ToString("MMMM yyyy"));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Aktuální okamžik - vlastnost Now
+            DateTime dt = DateTime.Now;
+            MessageBox.Show("Aktuální čas a datum pomocí now je: "+dt.ToString());
+            //Aktuální datum (den), čas nás nezajímá - vlastnost Today
+            dt = DateTime.Today;
+            MessageBox.Show("Aktuální čas a datum pomocí today je: "+dt.ToString());
+            
+        }
+         
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Výpočet věk z data narození - přesně na den
+
+            DateTime narozen = DateTime.Parse(textBox1.Text);
+            DateTime dnes = DateTime.Today;
+
+            //věk pouze přibližně = kolik let letos dovrší
+            int bDay = narozen.Year;
+            int today = dnes.Year;
+            int age = today - bDay;
+            //Věk chceme PŘESNĚ
+            //Spočteme datum letošních narozenin - metoda AddYears zvětší instanci Datetime o zadaný počet let
+            DateTime bDayThisYear = narozen.AddYears(age);
+            //
+            //Pokud ještě letos neměl narozeniny, příslušný věk zatím nedovršil a snížíme jej tedy o 1 rok
+            if (bDayThisYear > dnes) age--;
+                MessageBox.Show(age+" let");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Dovršení plnoletosti (18 let) nebo jiného věku
+            DateTime narozen = DateTime.Parse(textBox2.Text);
+            DateTime dnes = DateTime.Today;
+
+            //Zjistíme datum 18-tých narozenin
+            //narozeniny18 - datum 18-tých narozenin
+            DateTime narozeniny18 = narozen.AddYears(18);
+            //Zeptáme se, zda už měl 18-té narozeniny (dříve nebo dnes)
+
+            if (dnes >= narozeniny18) MessageBox.Show("Je plnoletý");
+            else MessageBox.Show("Není plnoletý");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            MessageBox.Show(dt.Day.ToString()); //den
+            MessageBox.Show(dt.DayOfWeek.ToString()); //den v týdnu
+            MessageBox.Show(dt.Month.ToString()); // měsíc
+            MessageBox.Show(dt.Year.ToString());//rok
+            MessageBox.Show(dt.Date.ToString()); // jenom datum
+            MessageBox.Show(dt.Date.ToString("d MMMM yy"));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            DateTime datum = DateTime.Parse(textBox3.Text);
+
+            //spočteme datum za 30 dní od zadaného data
+            DateTime datumZa30 = datum;
+            datumZa30 = datum.AddDays(30);
+            //Zeptáme se, zda spočtené datum už nastalo
+
+            if (datumZa30 >= DateTime.Today) MessageBox.Show("Karanční doba nevypršela");
+            else MessageBox.Show("Karanční doba vypršela");
+            //podobným způsobem se můžeme ptát, zda již uplynuly např. dvě hodiny od zadaného datumu a času (nebo minutz, sekundy..)
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // ***** Třída Timespan *****
+            //Slouží pro uchování časového intervalu - doba uplynulá mezi dvěma zadanými okamžiky
+            //Pomocí metod můžeme časový interval převádět na různé jednotky (sekundy, minuty, dny apod.)
+            //Převádět časový interval na měsíce a roky nelze - nejsou to stejně dlouhé časové jednotky)
+
+            DateTime dt1 = DateTime.Parse(textBox4.Text);
+            DateTime dt2 = DateTime.Parse(textBox5.Text);
+
+            
+
+            //long x = TimeSpan.TicksPerSecond;
+            //MessageBox.Show(x.ToString());
+        }
+    }
+}
