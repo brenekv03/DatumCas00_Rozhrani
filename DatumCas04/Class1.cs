60f84af76f0afb8c52ef28b526114eeb114196fd
brenekv.03 <brenekv.03@A321-15>
2022-03-25 08:18:05 +0100

    DatumCas04 + založení 05

---
+++ DatumCas04/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatumCas04
+{
+    class Class1
+    {
+        public static int PocetDnu(DateTime prodano, int pocetLetZaruky)
+        {
+            DateTime posledniDenZaruky = prodano.AddYears(pocetLetZaruky);
+            DateTime dt = DateTime.Now;
+
+            TimeSpan zbyva = posledniDenZaruky - dt;
+
+            return (int)zbyva.TotalDays;
+
+        }
+    }
+}
---
+++ DatumCas04/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatumCas04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            MessageBox.Show(Class1.PocetDnu(dateTimePicker1.Value, x).ToString());
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
