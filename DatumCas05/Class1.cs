75a53c683e19b19faee82217ed117f491a7586eb
endurovojta173 <vojtechbrenek173@gmail.com>
2022-03-29 18:56:56 +0200

    DatumCas05

---
+++ DatumCas05/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatumCas05
+{
+    class Class1
+    {
+        public static bool MinTrvanlivostDny(DateTime prodano, int pocetDniZaruky, int cena, out int sleva)
+        {
+
+            DateTime posledniDenZaruky = prodano.AddDays(pocetDniZaruky);
+            DateTime dt = DateTime.Now;
+            TimeSpan zbyva = posledniDenZaruky - dt;
+            bool prodejne = true;
+            sleva = 0;
+            if ((int)zbyva.TotalDays > 3)
+            {
+                sleva = cena;
+                prodejne = true;
+            }
+            if ((int)zbyva.TotalDays <= 3)
+            {
+                sleva = (cena * 75) / 100;
+                prodejne = true;
+            }
+            if ((int)zbyva.TotalDays < 0)
+            {
+                sleva = (cena * 50) / 100;
+                prodejne = true;
+            }
+            if ((int)zbyva.TotalDays < -10)
+            {
+                prodejne = false;
+            }
+
+            return prodejne;
+
+        }
+    }
+}
--- DatumCas05/Form1.cs
+++ DatumCas05/Form1.cs
@@ -16,5 +16,20 @@ namespace DatumCas05
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            int sleva = 0;
+            if (Class1.MinTrvanlivostDny(dateTimePicker1.Value, x, y, out sleva))
+            {
+                MessageBox.Show("Ano, je prodejne" + "cena je " + sleva);
+            }
+            else
+            {
+                MessageBox.Show("Zbozi neni prodejne");
+            }
+        }
     }
 }
